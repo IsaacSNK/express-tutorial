db5bb9326da4d286faef81a47528a96483b1529b
Isaac Ramirez Herrera <joseisaacr@microsoft.com>
2022-03-05 15:16:33 -0600

    New change

--- src/infrastructure/DB.js
+++ src/infrastructure/DB.js
@@ -25,7 +25,6 @@ export const DB = [
         "https://swapi.dev/api/starships/12/",
         "https://swapi.dev/api/starships/22/"
       ],
-      "created": "2014-12-09T13:50:51.644000Z",
       "edited": "2014-12-20T21:17:56.891000Z",
       "url": "https://swapi.dev/api/people/1/"
     },
